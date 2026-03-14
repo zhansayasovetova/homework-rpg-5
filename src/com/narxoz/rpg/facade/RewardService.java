7ea8ced07be10b3abe40e7680bda10a8419840da
zhansayasovetova <zhansaya.sovetova@narxoz.kz>
2026-03-14 21:41:35 +0500

    Implement RewardService to determine dungeon rewards

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,18 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
+        if (battleResult == null || battleResult.getWinner() == null) {
+            return "No reward";
         }
-        return "TODO";
+
+        if ("Draw".equals(battleResult.getWinner())) {
+            return "Small treasure chest";
+        }
+
+        if ("Arthas".equals(battleResult.getWinner())) {
+            return "Epic dungeon loot";
+        }
+
+        return "No reward";
     }
 }
